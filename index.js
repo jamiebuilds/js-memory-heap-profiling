0b1bd3fd8d03a7c96ca5f76eb586ec5a728c980b
Jamie Kyle <me@thejameskyle.com>
2018-04-02 15:44:17 -0700

    make pretty

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const memwatch = require('memwatch-next');
+const prettyBytes = require('pretty-bytes');
 
-function record(count, fn) {
+function row(name, size, count, total) {
+  return (
+    '| ' +
+    name.padStart(30) +
+    ' | ' +
+    size.padEnd(10) +
+    ' | ' +
+    count.padStart(10) +
+    ' | ' +
+    total.padStart(10) +
+    ' |'
+  );
+}
+
+let title = row('kind', 'total/n', 'n', 'total');
+console.log(title);
+console.log('-'.repeat(title.length));
+
+function record(name, count, fn) {
   memwatch.gc();
   let hd = new memwatch.HeapDiff();
   let arr = new Array();
   for (let i = 0; i < count; i++) arr.push(fn());
   let diff = hd.end();
-  return diff.change.size_bytes / count;
+  let total = diff.change.size_bytes;
+  console.log(row(name, prettyBytes(total / count), String(count), prettyBytes(total)));
 }
 
 function fill(count, obj, fn) {
@@ -14,59 +34,62 @@ function fill(count, obj, fn) {
   return obj;
 }
 
-let results = {};
-
 let i = 0;
 
 // Sanity checks:
 
-results['number'] = record(1000000, () => i++);
-results['empty string'] = record(1000000, () => "");
-results['true'] = record(1000000, () => true);
+record('number', 1000000, () => i++);
+record('empty string', 1000000, () => "");
+record('true', 1000000, () => true);
 
 // Tests:
 
-results['empty object'] = record(1000000, () => new Object());
-results['empty array'] = record(1000000, () => new Array());
-results['empty map'] = record(1000000, () => new Map());
-results['empty set'] = record(1000000, () => new Set());
+record('empty object', 1000000, () => new Object());
+record('empty array', 1000000, () => new Array());
+record('empty map', 1000000, () => new Map());
+record('empty set', 1000000, () => new Set());
+record('empty weakmap', 1000000, () => new WeakMap());
+record('empty weakset', 1000000, () => new WeakSet());
+
+
+record('Object.create(null)', 1000000, () => Object.create(null));
 
 function objSet(obj, i) { obj[i] = 0; }
 function arrSet(arr, i) { arr.push(0); }
 function mapSet(map, i) { map.set(i, 0); }
 function setSet(set, i) { set.add(i); }
 
-results['10 item object'] = record(100000, () => fill(10, new Object(), objSet));
-results['10 item array'] = record(100000, () => fill(10, new Array(), arrSet));
-results['10 item map'] = record(100000, () => fill(10, new Map(), mapSet));
-results['10 item set'] = record(100000, () => fill(10, new Set(), setSet));
+record('10 item object', 100000, () => fill(10, new Object(), objSet));
+record('10 item array', 100000, () => fill(10, new Array(), arrSet));
+record('10 item map', 100000, () => fill(10, new Map(), mapSet));
+record('10 item set', 100000, () => fill(10, new Set(), setSet));
 
-results['100 item object'] = record(10000, () => fill(100, new Object(), objSet));
-results['100 item array'] = record(10000, () => fill(100, new Array(), arrSet));
-results['100 item map'] = record(10000, () => fill(100, new Map(), mapSet));
-results['100 item set'] = record(10000, () => fill(100, new Set(), setSet));
+record('100 item object', 10000, () => fill(100, new Object(), objSet));
+record('100 item array', 10000, () => fill(100, new Array(), arrSet));
+record('100 item map', 10000, () => fill(100, new Map(), mapSet));
+record('100 item set', 10000, () => fill(100, new Set(), setSet));
 
-results['1000 item object'] = record(1000, () => fill(1000, new Object(), objSet));
-results['1000 item array'] = record(1000, () => fill(1000, new Array(), arrSet));
-results['1000 item map'] = record(1000, () => fill(1000, new Map(), mapSet));
-results['1000 item set'] = record(1000, () => fill(1000, new Set(), setSet));
+record('1000 item object', 1000, () => fill(1000, new Object(), objSet));
+record('1000 item array', 1000, () => fill(1000, new Array(), arrSet));
+record('1000 item map', 1000, () => fill(1000, new Map(), mapSet));
+record('1000 item set', 1000, () => fill(1000, new Set(), setSet));
 
 function objDef(obj, i) {
   Object.defineProperty(obj, i, { get: getter });
 }
 
-results['10 item object defProp'] = record(100000, () => fill(10, new Object(), objDef));
-results['100 item object defProp'] = record(10000, () => fill(100, new Object(), objDef));
-results['1000 item object defProp'] = record(1000, () => fill(1000, new Object(), objDef));
+record('10 item object defProp', 100000, () => fill(10, new Object(), objDef));
+record('100 item object defProp', 10000, () => fill(100, new Object(), objDef));
+record('1000 item object defProp', 1000, () => fill(1000, new Object(), objDef));
 
 function getter() { return 0; }
 function objDefRef(obj, i) {
   Object.defineProperty(obj, i, { get: getter });
 }
 
-results['10 item object defProp ref'] = record(100000, () => fill(10, new Object(), objDef));
-results['100 item object defProp ref'] = record(10000, () => fill(100, new Object(), objDef));
-results['1000 item object defProp ref'] = record(1000, () => fill(1000, new Object(), objDef));
+record('10 item object defProp ref', 100000, () => fill(10, new Object(), objDef));
+record('100 item object defProp ref', 10000, () => fill(100, new Object(), objDef));
+record('1000 item object defProp ref', 1000, () => fill(1000, new Object(), objDef));
 
 let shared = {};
 
@@ -74,9 +97,9 @@ function objShared(obj, i) {
   obj[i] = shared;
 }
 
-results['10 item object shared obj'] = record(100000, () => fill(10, new Object(), objShared));
-results['100 item object shared obj'] = record(10000, () => fill(100, new Object(), objShared));
-results['1000 item object shared obj'] = record(1000, () => fill(1000, new Object(), objShared));
+record('10 item object shared obj', 100000, () => fill(10, new Object(), objShared));
+record('100 item object shared obj', 10000, () => fill(100, new Object(), objShared));
+record('1000 item object shared obj', 1000, () => fill(1000, new Object(), objShared));
 
 function arrMixed(arr, i) {
   switch (i % 4) {
@@ -87,8 +110,6 @@ function arrMixed(arr, i) {
   }
 }
 
-results['10 item arr mixed'] = record(100000, () => fill(10, new Array(), arrMixed));
-results['100 item arr mixed'] = record(10000, () => fill(100, new Array(), arrMixed));
-results['1000 item arr mixed'] = record(1000, () => fill(1000, new Array(), arrMixed));
-
-console.log(results);
+record('10 item arr mixed', 100000, () => fill(10, new Array(), arrMixed));
+record('100 item arr mixed', 10000, () => fill(100, new Array(), arrMixed));
+record('1000 item arr mixed', 1000, () => fill(1000, new Array(), arrMixed));
